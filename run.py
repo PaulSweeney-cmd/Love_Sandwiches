3ff9f253ec8a7b55e02c8e4fb5f819fdb49af6aa
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-05-15 12:13:22 +0000

    Adjusted validation to check values are integers

--- run.py
+++ run.py
@@ -44,6 +44,7 @@ def validate_data(values):
     or if there are more or less than six values
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
