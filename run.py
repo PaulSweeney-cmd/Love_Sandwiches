e61dd0df0a474d1e751e2ab5b291f2557fe317eb
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-05-15 11:49:25 +0000

    Added try and except statements to check for valid data enterd by the user

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 # Accessing the google spreadsheet
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-
+# Creating function to get data string from user
 def get_sales_data():
     """
     Get sales figures input from the user
@@ -30,8 +30,25 @@ def get_sales_data():
     print("For example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
 
-    
+    # Using the .split() method splits the strings up at the commas, turning it in to a list value
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+# Creating a function to valiudate the user data
+def validate_data(values):
+    """
+    Inside the try statement, convert all string values to integers, 
+    Raises ValueError if strings cannot be converted to integers,
+    or if there are more or less than six values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 get_sales_data()
-
