34c19c53e11b532d36abf13cbed4f30401a68e5e
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-05-16 12:33:18 +0000

    Added a function t calculate the average and apply 10%

--- run.py
+++ run.py
@@ -144,14 +144,30 @@ def get_last_5_entries_sales():
     and returns the data as a list of lists.
     """
     sales = SHEET.worksheet("sales")
-    
+
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
     return columns
-        
 
+
+
+
+def calculate_stock_data(data):
+    """
+    Calculates the average stock for each sandwich type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 
 
@@ -167,12 +183,16 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
+    
+
 
 
 
 
 
 print("Welcome To Love Sandwiches Data Automation!\n")
-# main()
+main()
 
-sales_columns = get_last_5_entries_sales()
